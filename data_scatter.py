d6379335f554fc722ea4ce1be62090c9a5b09f09
ModeuSProgrammer <niffty999@gmail.com>
2026-05-31 23:07:04 +0300

    Added a schedule and save

--- data_scatter.py
+++ data_scatter.py
@@ -1,94 +1,111 @@
 import datatest
+from datetime import datetime
 import io
 import tkinter as tk
 from matplotlib import figure
 from PIL import Image, ImageTk
 
-current_x_index = 1
-current_y_index = 2
 try:
-    df = datatest.df
-    # Создание главного окна
-    window = tk.Tk()
-
-    # Заполнение меню осей xy
+    # Заполнение меню осей x и y
     def nav_xy_interface(columns):
-        frame_y = tk.Frame(borderwidth=1, relief=tk.SOLID, padx=5, pady=5)
-        frame_y.pack(side=tk.LEFT, fill=tk.Y)
-        frame_x = tk.Frame(borderwidth=1, relief=tk.SOLID, padx=5, pady=5)
-        frame_x.pack(side=tk.BOTTOM, fill=tk.X)
         for col in columns:
-            btn_y = tk.Button(frame_y, text=col, padx=5, pady=5)
-            btn_y.pack(anchor=tk.NW)
+            btn_y = tk.Button(frame_y, text=col, padx=10, pady=5)
+            btn_y.pack(anchor=tk.NW, fill="x")
             btn_y.axis = "Y"
-            btn_y.bind("<Button-1>", on_click)
-            btn_x = tk.Button(frame_x, text=col, padx=5, pady=5)
+            btn_y.bind("<Button-1>", on_click_menu)
+            btn_x = tk.Button(frame_x, text=col, padx=10, pady=5)
             btn_x.pack(side=tk.LEFT)
             btn_x.axis = "X"
-            btn_x.bind("<Button-1>", on_click)
+            btn_x.bind("<Button-1>", on_click_menu)
 
-    # Заполнение значений
+
+    # Формирование списка значений
     def nav_add_xy_values():
-        part_df = df.drop(columns=['title', 'artist/s', 'key', 'mode', 'time_signature'])
         columns = (list(part_df.columns)[1:])
         nav_xy_interface(columns)
 
 
-    # Выбор значения меню
-    def btn_switch_menu(title, flag):
+    # Изменяет значение графика
+    def btn_switch_graph(title, flag):
         global current_x_index, current_y_index
-        part_df = df.drop(columns=['title', 'artist/s', 'key', 'mode', 'time_signature'])
         col_index = part_df.columns.get_loc(title)
-        print(col_index)
         match flag:
             case 'X':
-               current_x_index = col_index
+                current_x_index = col_index
             case 'Y':
-               current_y_index = col_index
+                current_y_index = col_index
 
 
-    def on_click(event):
+    # Отслеживает нажатие на кнопки меню
+    def on_click_menu(event):
         btn_text = event.widget.cget("text")
         btn_axis = event.widget.axis
-        btn_switch_menu(btn_text, btn_axis)
-        print(f"Вы нажали: {btn_text} {btn_axis}")
+        btn_switch_graph(btn_text, btn_axis)
         update_image()
 
+
+    # Обновление графика
     def update_image():
         global image
-        image = get_scatter_as_photoImage()
+        image = get_scatter_as_photoimage()
         canvas.delete("all")
         canvas.create_image(0, 0, anchor=tk.NW, image=image)
 
+
     # Работа с отображением графиков
-    def get_scatter_as_photoImage():
-        global current_x_index, current_y_index,image
-        fig = figure.Figure()
+    def get_scatter_as_photoimage():
+        global current_x_index, current_y_index, fig
+        fig = figure.Figure(layout="constrained")
         ax = fig.add_subplot()
-        part_df = df.drop(columns=['title', 'artist/s', 'key', 'mode', 'time_signature'])
         fig.supxlabel(part_df.columns[current_x_index])
         fig.supylabel(part_df.columns[current_y_index])
-        ax.scatter([0], [0])
+        x_column = part_df.iloc[:, current_x_index]
+        y_column = part_df.iloc[:, current_y_index]
+        ax.scatter(x_column, y_column, marker=">")
         buf = io.BytesIO()
-        fig.savefig(buf)
-        image = Image.open(buf)
-        return ImageTk.PhotoImage(image)
+        fig.savefig(buf, format="png")
+        buf.seek(0)
+        return ImageTk.PhotoImage(Image.open(buf))
 
+    def save_click(event):
+        global fig
+        if fig is None:
+            return
+        now = datetime.now()
+        time_formatted = now.strftime("%H_%M_%S")
+        filename = f"graph_{time_formatted}.png"
+        fig.savefig(filename)
+
+    window = tk.Tk()
+    df = datatest.df
+    part_df = df.drop(columns=['title', 'artist/s', 'key', 'mode', 'time_signature'])
+    # Глобальные выбранные значение x и y
+    current_x_index = 1
+    current_y_index = 2
+
+    # Создание областей для меню x и y
+    frame_y = tk.Frame(borderwidth=1, relief=tk.SOLID, padx=5, pady=5)
+    frame_y.pack(side=tk.LEFT, fill=tk.Y)
+    frame_x = tk.Frame(borderwidth=1, relief=tk.SOLID, padx=5, pady=5)
+    frame_x.pack(side=tk.BOTTOM, fill=tk.X)
+
+    # Создание области для расположения доп. кнопок
+    frame_settings = tk.Frame(frame_y)
+    frame_settings.pack(side=tk.BOTTOM, fill="x")
+    btn_save = tk.Button(frame_settings, text="Сохранить", padx=5, pady=5)
+    btn_save.pack(fill="x")
+    btn_save.bind("<Button-1>", save_click)
 
     # Вызов функций связанных с отображением информации
     nav_add_xy_values()
-    canvas = tk.Canvas(width=640, height=480)
-    canvas.pack(side=tk.LEFT)
 
-    # Добавление изображение графика
-    image = get_scatter_as_photoImage()
+    canvas = tk.Canvas(width=740, height=480)
+    canvas.pack(side=tk.LEFT)
+    # Добавление изображение графика при иницилизации приложения
+    image = get_scatter_as_photoimage()
     canvas.create_image(0, 0, anchor=tk.NW, image=image)
 
-    # Отображение главного окна и его ограничение
     window.resizable(False, False)
     window.mainloop()
-
 except Exception as e:
     print(f"Произошла непредвиденная ошибка: {e}")
-
-
