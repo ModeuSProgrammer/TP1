ccfb36d996a51ca4661fb5325ed6d59374d5eb21
ModeuSProgrammer <niffty999@gmail.com>
2026-06-01 18:43:05 +0300

    Added func get titles category

--- data_visual.py
+++ data_visual.py
@@ -1,1 +1,16 @@
-#
+import data_scatter
+
+
+try:
+    df = data_scatter.df
+    part_category_df = df[['title', 'artist/s', 'key', 'mode', 'time_signature']]
+
+    def get_category_titles():
+        titles = list(part_category_df[:1])
+        print(titles)
+
+
+    get_category_titles()
+
+except Exception as e:
+    print(f"Произошла непредвиденная ошибка: {e}")
